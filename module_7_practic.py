2c721cad408091e8172f5e993dc4f3a243bd7345
Vyacheslav <slawushkin@mail.ru>
2024-08-05 23:01:54 +0300

    Update module_7_practic.py

--- module_7_practic.py
+++ module_7_practic.py
@@ -1,38 +1,97 @@
 import tkinter
 import os
-from tkinter import filedialog
+from tkinter import filedialog as fd
 
 
 def file_select():
-    filename = filedialog.askopenfilename(initialdir='/',
-                                          title='Select a file',
-                                          filetypes=(('File', '.txt'),)
-                                          )
+    filename = fd.askopenfilename(initialdir='/',
+                                  title='Выбрать файл',
+                                  filetypes=(('Файл', '.txt'),)
+                                  )
     text['text'] += "  " + filename
     os.startfile(filename)
 
 
+def close_wind():
+    window.destroy()
+
+
 window = tkinter.Tk()
 window.title('Проводник')
-window.geometry('395x175')
-window.configure(bg='black')
+window.geometry('353x220+300+300')
+window.configure(bg='gray25')
 window.resizable(False, False)
-text = tkinter.Label(window,
-                     text='File',
-                     height=5,
-                     width=56,
-                     bg='silver',
-                     fg='red'
+
+text = tkinter.Label(text='Файл',
+                     height=4,
+                     width=49,
+                     background='white',
+                     fg='blue',
+                     bd=4,
+                     relief=tkinter.RIDGE
                      )
-text.grid(column=1, row=1)
-button = tkinter.Button(window,
-                        height=5,
-                        width=15,
-                        bg='silver',
-                        fg='blue',
-                        text='Select a file',
-                        command=file_select
-                        )
-button.grid(column=1, row=2, pady=5)
+text.place(x=0, y=55)
+
+button_select = tkinter.Button(text='Выбрать файл',
+                               height=3,
+                               width=20,
+                               bg='silver',
+                               fg='blue',
+                               cursor="hand2",
+                               bd=4,
+                               activebackground='gold',
+                               relief=tkinter.RAISED,
+                               command=file_select
+                               )
+button_select.place(x=0, y=150)
+
+button_select_1 = tkinter.Button(text='Закрыть окно',
+                                 height=3,
+                                 width=20,
+                                 bg='silver',
+                                 fg='blue',
+                                 cursor="hand2",
+                                 bd=4,
+                                 activebackground='gold',
+                                 relief=tkinter.RAISED,
+                                 command=close_wind
+                                 )
+button_select_1.place(x=199, y=150)
+
+button_select_2 = tkinter.Button(text='info',
+                                 height=1,
+                                 width=10,
+                                 pady=5,
+                                 background='silver',
+                                 fg='blue',
+                                 bd=4,
+                                 cursor="hand2",
+                                 relief=tkinter.RAISED
+                                 )
+button_select_2.place(x=0, y=1)
+
+button_select_3 = tkinter.Button(text='version',
+                                 height=1,
+                                 width=10,
+                                 pady=5,
+                                 background='silver',
+                                 fg='blue',
+                                 bd=4,
+                                 cursor="hand2",
+                                 relief=tkinter.RAISED
+                                 )
+button_select_3.place(x=135, y=1)
+
+button_select_4 = tkinter.Button(text='admin',
+                                 height=1,
+                                 width=10,
+                                 pady=5,
+                                 background='silver',
+                                 fg='blue',
+                                 bd=4,
+                                 cursor="hand2",
+                                 relief=tkinter.RAISED
+                                 )
+button_select_4.place(x=270, y=1)
 
 window.mainloop()
