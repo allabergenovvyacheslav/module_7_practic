6355707e5636c400ca7896350e8b09c1ce1366af
Vyacheslav <slawushkin@mail.ru>
2024-08-06 14:40:23 +0300

    Update module_7_practic.py

--- module_7_practic.py
+++ module_7_practic.py
@@ -1,6 +1,13 @@
 import tkinter
 import os
 from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+window = tkinter.Tk()
+window.title('Проводник')
+window.geometry('353x220+300+300')
+window.configure(bg='gray25')
+window.resizable(False, False)
 
 
 def file_select():
@@ -16,11 +23,18 @@ def close_wind():
     window.destroy()
 
 
-window = tkinter.Tk()
-window.title('Проводник')
-window.geometry('353x220+300+300')
-window.configure(bg='gray25')
-window.resizable(False, False)
+def info_wind():
+    showinfo(title='info', message='Инструкции работы окна Проводник:''\n'
+                                   '1. нажмите на кнопку Выбор фала''\n'
+                                   '2. откроется диск для выбора файлов''\n'
+                                   '3. выберите файл')
+
+
+def admin_wind():
+    showinfo(title='admin', message='Вячеслав Аллабергенов''\n'
+                                    "Курс Python - разработчик"'\n'
+                                    'Университет Urban')
+
 
 text = tkinter.Label(text='Файл',
                      height=4,
@@ -66,21 +80,22 @@ button_select_2 = tkinter.Button(text='info',
                                  fg='blue',
                                  bd=4,
                                  cursor="hand2",
-                                 relief=tkinter.RAISED
+                                 relief=tkinter.RAISED,
+                                 command=info_wind
                                  )
 button_select_2.place(x=0, y=1)
 
-button_select_3 = tkinter.Button(text='version',
-                                 height=1,
-                                 width=10,
-                                 pady=5,
-                                 background='silver',
-                                 fg='blue',
-                                 bd=4,
-                                 cursor="hand2",
-                                 relief=tkinter.RAISED
-                                 )
-button_select_3.place(x=135, y=1)
+# button_select_3 = tkinter.Button(text='version',
+#                                  height=1,
+#                                  width=10,
+#                                  pady=5,
+#                                  background='silver',
+#                                  fg='blue',
+#                                  bd=4,
+#                                  cursor="hand2",
+#                                  relief=tkinter.RAISED
+#                                  )
+# button_select_3.place(x=135, y=1)
 
 button_select_4 = tkinter.Button(text='admin',
                                  height=1,
@@ -90,8 +105,8 @@ button_select_4 = tkinter.Button(text='admin',
                                  fg='blue',
                                  bd=4,
                                  cursor="hand2",
-                                 relief=tkinter.RAISED
+                                 relief=tkinter.RAISED,
+                                 command=admin_wind
                                  )
 button_select_4.place(x=270, y=1)
-
 window.mainloop()
